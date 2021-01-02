3c672393ea8a96bac7dea00262780104f56e721f
Brunopvh <brunodasill@gmail.com>
2021-01-02 09:50:03 -0400

    Melhorias

--- epsxe-gui.py
+++ epsxe-gui.py
@@ -8,25 +8,25 @@ Windows e Linux.
 
 import sys, os, subprocess
 import sys
-from PyQt5.QtWidgets import (QApplication, 
+from PyQt5.QtWidgets import (
+							QApplication, 
+							QAction,
+							qApp,
+							QFrame,
+                            QMainWindow,
 							QWidget, 
 							QPushButton, 
-							QLineEdit, 
+							QLineEdit,
+							QLabel, 
                             QHBoxLayout, 
                             QMessageBox,
                             QGroupBox,
                             QVBoxLayout,
                             QRadioButton,
                             QGridLayout,
+                            QStyle
                             )
 from PyQt5.QtGui import QIcon
-'''
-from PyQt5 import (
-					QtWidgets, 
-					QtGui, 
-					QtCore,
-					)
-'''
 
 
 __version__ = '2020-12-31'
@@ -45,60 +45,67 @@ elif utils.KERNEL_TYPE == 'Linux':
 	url_epsxe_zip = 'http://www.epsxe.com/files/ePSXe205linux.zip'
 	path_epsxe_zip_file = os.path.abspath(os.path.join(config.dir_cache, 'ePSXe205linux.zip'))
 
-class Window(QWidget):
+class EpsxeWindow(QWidget):
 	'''
 	https://medium.com/@wilfilho/pyqt5-o-fantastico-mundo-das-guis-62914b1b39c1
 	https://medium.com/@wilfilho/conhecendo-o-coracao-e-as-arterias-do-pyqt5-22ba4187531
 	'''
 	def __init__(self, parent=None):
-		super(Window, self).__init__()
+		super(EpsxeWindow, self).__init__()
 		self.window_icon = os.path.abspath(os.path.join(dir_of_executable, 'imgs', 'epsxe.png')) 
 		self.setWindowIcon(QIcon())
 		self.setWindowTitle('Epsxe-Qt-Gui')
-		self.resize(400, 480)
-		self.set_buttons()
+		#self.setStyleSheet("background-color: #CD853F;") # Setar cor de fundo verde.
+		self.setGeometry(150, 150, 550, 450)
+
+		self.set_buttons()	
+		#self.initUI()
 		self.text = ''
 		self.title_info = ''
 
-
 	def set_buttons(self):
-		
-		# Botões com caixa de seleção.
-		self.option_download = QRadioButton("Baixar")
-		self.option_download.setChecked(True)
+		# Botões com caixa de seleção		
 		self.option_installer = QRadioButton("Instalar")
+		self.option_installer.setChecked(True)
+		self.option_download = QRadioButton("Baixar")
+		self.option_configure = QRadioButton("Configurar")
 		self.option_uninstall = QRadioButton("Desinstalar")
 
 		# Box para Widgets
-		self.groupbox = QGroupBox("Selecione uma opção")
+		self.groupbox = QGroupBox("Selecione uma opção e clique em OK")
 
-		# Layout options.
+		# Layout para os botões de seleção.
 		self.layout_options = QVBoxLayout()
 		self.layout_options.addWidget(self.option_download)
 		self.layout_options.addWidget(self.option_installer)
+		self.layout_options.addWidget(self.option_configure)
 		self.layout_options.addWidget(self.option_uninstall)
-		self.groupbox.setLayout(self.layout_options)
+		self.groupbox.setLayout(self.layout_options) 
 
-		# Botão OK.
+		# Botões de clique.
 		self.button_ok = QPushButton('OK', self)
+		self.button_ok.setFixedWidth(110)
+		self.button_ok.setFixedHeight(25)
+		self.button_ok.setStyleSheet("border: 1px solid black;") 
+		self.button_ok.setStyleSheet('{background-color: #A3C1DA; border:  none}')
 		self.button_ok.clicked.connect(self.run)
+		self.buttonExit = QPushButton('Sair', self)
+		self.buttonExit.setFixedWidth(110)
+		self.buttonExit.setFixedHeight(25)
+		self.buttonExit.clicked.connect(self.click_exit_button)
 
-		# Layout para o botão OK.
+		# Layout para botões de clique.
 		self.layout_widgets_click = QHBoxLayout()
 		self.layout_widgets_click.addWidget(self.button_ok)
+		self.layout_widgets_click.addWidget(self.buttonExit)
 
 		# Main Layout
 		self.layout_master = QVBoxLayout()
-		#self.layout_master.addLayout(self.layout_widgets_click)
 		self.layout_master.addWidget(self.groupbox)
 		self.layout_master.addLayout(self.layout_widgets_click)
-		self.setLayout(self.layout_master)		
+		self.setLayout(self.layout_master)	
 
-		# Botão sair.
-		self.buttonExit = QPushButton('Sair', self)
-		self.buttonExit.clicked.connect(self.click_exit_button)
-		self.layout_master.addWidget(self.buttonExit)
-		self.setLayout(self.layout_master)
+		#QMainWindow().statusBar()
 
 	def run(self):
 		if self.option_download.isChecked():
@@ -108,6 +115,9 @@ class Window(QWidget):
 			epsxe_config.teste()
 			self.text = 'Ação para instalar o programa.'
 			self.title_info = 'Instalar'
+		elif self.option_configure.isChecked():
+			self.text = 'Ação para configurar o epsxe'
+			self.title_info = 'Configurar Epsxe'
 		elif self.option_uninstall.isChecked():
 			self.text = 'Ação para desinstalar o programa.'
 			self.title_info = 'Desinstalar'
@@ -127,6 +137,7 @@ class Window(QWidget):
 			self.messsage_box = QMessageBox.information(self, self.title_info, self.text)
 
 	def click_exit_button(self):
+		'''
 		exit_msg = QMessageBox.question(
 						self, 
 						'MessageBox', 
@@ -137,8 +148,10 @@ class Window(QWidget):
 		if exit_msg == QMessageBox.Yes:
 			print('QMessageBox.Yes: saindo')
 			sys.exit()
+		'''
+		sys.exit()
 
 root = QApplication(sys.argv)
-app = Window()
+app = EpsxeWindow()
 app.show()
 sys.exit(root.exec_())
